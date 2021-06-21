54ed2c2ca59ae1ef51c27a85bdef03e6ea5fa4a5
Fedor Dudinskiy <fdudinsk@redhat.com>
2021-06-21 14:00:02 +0200

    +Using json

--- src/main/java/org/acme/ReactiveGreetingResource.java
+++ src/main/java/org/acme/ReactiveGreetingResource.java
@@ -2,12 +2,15 @@ package org.acme;
 
 import io.smallrye.mutiny.Multi;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ReactiveGreetingResource {
 
     @GET
@@ -17,7 +20,6 @@ public class ReactiveGreetingResource {
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Path("multi")
     public Multi<String> multi() {
         return Multi.createFrom().items("Hello", "Hola");
