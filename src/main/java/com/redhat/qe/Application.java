5776844269d06a6504f27ceff7d59ba3041eb4ad
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-09-14 14:41:10 +0200

    An odd parity example

--- src/main/java/com/redhat/qe/Application.java
+++ src/main/java/com/redhat/qe/Application.java
@@ -1,37 +1,29 @@
 package com.redhat.qe;
 import io.quarkus.qute.Location;
+import io.quarkus.qute.Qute;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import java.util.Arrays;
 
 import static java.util.Objects.requireNonNull;
 
 @Path("")
 public class Application {
 
-    @Inject
-    Template basic;
-
     @Location("basic.html")
-    Template located;
+    Template template;
 
     @GET
-    @Path("/base")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance base() {
-        return addData(basic);
-    }
-
-    @GET
-    @Path("/located")
+    @Path("/test")
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance located() {
-        return addData(located);
+        return addData(template);
     }
 
     private TemplateInstance addData(Template template) {
