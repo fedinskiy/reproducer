e4482c4d37d992b5e5cca4d289feb1b8340ef200
Fedor Dudinskiy <fdudinsk@redhat.com>
2023-02-28 18:04:38 +0100

    Reproducer for Hibernate sequences with negative values

---
+++ src/test/java/io/quarkus/qe/multiplepus/MultiplePersistenceUnitIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.qe.multiplepus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MultiplePersistenceUnitIT extends MultiplePersistenceUnitTest {
+}
