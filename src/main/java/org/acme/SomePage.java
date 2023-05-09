80cd807532ec727b2109e817dac5735e0d857f0c
Fedor Dudinskiy <fdudinsk@redhat.com>
2023-05-09 12:32:22 +0200

    Reproducer for grpc probes

--- src/main/java/org/acme/SomePage.java
+++ src/main/java/org/acme/SomePage.java
@@ -1,29 +1,21 @@
 package org.acme;
 
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 import static java.util.Objects.requireNonNull;
 
 @Path("/some-page")
 public class SomePage {
 
-    private final Template page;
-
-    public SomePage(Template page) {
-        this.page = requireNonNull(page, "page is required");
-    }
-
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+    public String get(@QueryParam("name") String name) {
+        return "Hello, "+name;
     }
 
 }
