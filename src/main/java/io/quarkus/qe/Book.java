39f41d575a633a069efe07aa9bc41fe9bea18a26
Fedor Dudinskiy <fdudinsk@redhat.com>
2023-06-09 16:39:48 +0200

    Reproducer4 for devmode-podman incompatibility

---
+++ src/main/java/io/quarkus/qe/Book.java
@@ -0,0 +1,26 @@
+package io.quarkus.qe;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "book")
+public class Book extends PanacheEntityBase {
+
+    @Id
+    @SequenceGenerator(name = "bookSequence", sequenceName = "SEQ_Book", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bookSequence")
+    public Long id;
+
+    @NotBlank(message = "book title must be set")
+    public String title;
+
+    @NotBlank(message = "book author must be set")
+    public String author;
+}
