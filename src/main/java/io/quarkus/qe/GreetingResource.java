bd700ea787ddeab3733d5354375292e6612eb773
Fedor Dudinsky <fdudinsk@redhat.com>
2024-06-12 16:38:07 +0200

    syslog.max-length ignores message length

--- src/main/java/io/quarkus/qe/GreetingResource.java
+++ src/main/java/io/quarkus/qe/GreetingResource.java
@@ -4,20 +4,24 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 @Path("/")
 public class GreetingResource {
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String root() {
+        LOG.info("Root access");
         return "Yes, it works";
     }
 
     @GET
-    @Path("/hello")
+    @Path("/long")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String longMessage() {
+        LOG.info("Message, which is very long and is not expected to fit into 64 bytes");
         return "Hello from RESTEasy Reactive";
     }
 }
