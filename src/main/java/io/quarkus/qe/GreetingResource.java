3bd7c67859f8f80da74d2f1ff4ec9996eca9b2f3
Fedor Dudinsky <fdudinsk@redhat.com>
2025-02-19 17:22:22 +0100

    Reproducer for lack of serialization classes

--- src/main/java/io/quarkus/qe/GreetingResource.java
+++ src/main/java/io/quarkus/qe/GreetingResource.java
@@ -2,8 +2,10 @@ package io.quarkus.qe;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/")
 public class GreetingResource {
@@ -19,5 +21,24 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path("/book")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book book() {
+        return new Book("Catch-22", "Joseph Heller");
+    }
+
+    @GET
+    @Path("/class/{name}")
+    public Response getClass(@PathParam("name") String className) {
+        try {
+            Class<?> existingclass = Class.forName(className);
+            return Response.ok(existingclass.getCanonicalName()).build();
+        } catch (ClassNotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND.getStatusCode(),
+                    "There is no such class: " + className).build();
+        }
     }
 }
--- src/test/java/io/quarkus/qe/GreetingResourceTest.java
+++ src/test/java/io/quarkus/qe/GreetingResourceTest.java
@@ -1,10 +1,14 @@
 package io.quarkus.qe;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -16,6 +20,35 @@ public class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    public void testBookEndpoint() {
+        Response response = given()
+                .when().get("/book");
+        assertEquals(200, response.statusCode());
+        ResponseBody body = response.body();
+        assertEquals("Joseph Heller", body.jsonPath().getString("author"), body.asString());
+        assertEquals("Catch-22", body.jsonPath().getString("title"), body.asString());
+    }
+
+    @Test
+    public void classTest() {
+        given()
+                .get("/class/io.quarkus.qe.Book")
+                .then()
+                .statusCode(200)
+                .body(containsString("Book"));
+        given()
+                .get("/class/io.quarkus.qe.Shmook")
+                .then()
+                .statusCode(404);
+
+        given()
+                .get("/class/io.quarkus.qe.Book$quarkusjacksonserializer")
+                .then()
+                .statusCode(200)
+                .body(containsString("Book$quarkusjacksonserializer"));
     }
 
 }
