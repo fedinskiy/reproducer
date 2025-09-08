36586b4785519541cba2a1e98c7a6dbed0848667
Fedor Dudinsky <fdudinsk@redhat.com>
2025-09-08 14:54:43 +0200

    Unexpected beahavior for chunking

--- src/main/java/io/quarkus/qe/GreetingResource.java
+++ src/main/java/io/quarkus/qe/GreetingResource.java
@@ -6,6 +6,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Path("/")
 public class GreetingResource {
 
@@ -16,8 +21,25 @@ public class GreetingResource {
     }
 
     @GET
-    @Path("/hello")
-    public Response hello() {
-        return Response.ok("hello").header("Transfer-Encoding", "chunked").build();
+    @Path("/text")
+    @io.quarkus.vertx.http.Uncompressed
+    public Response text() {
+        String content = createText();
+        return Response.ok(content).build();
+    }
+
+    @GET
+    @Path("/stream")
+    public Response stream() {
+        String content = createText();
+        var stream=new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+        return Response.ok(stream).build();
+    }
+
+    private static String createText() {
+        String content = IntStream.range(1, 100)
+                .mapToObj(i -> "Hello no." + i)
+                .collect(Collectors.joining("\n"));
+        return content;
     }
 }
