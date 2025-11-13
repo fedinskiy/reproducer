85f610ff3285541a9bce968f6c7020f1e2ef1a93
Fedor Dudinsky <fdudinsk@redhat.com>
2025-11-13 15:39:13 +0100

    Reproducer for https://github.com/quarkusio/quarkus/issues/50322

--- src/main/java/io/quarkus/qe/GreetingResource.java
+++ src/main/java/io/quarkus/qe/GreetingResource.java
@@ -4,9 +4,13 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/")
 public class GreetingResource {
+
+    @ConfigProperty(name = "quarkus.http-name")
+    String name;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -18,6 +22,6 @@ public class GreetingResource {
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello " + name;
     }
 }
