b96859d7c0df7de6b476bf9db22a17853886dacd
Fedor Dudinsky <fdudinsk@redhat.com>
2026-03-30 09:12:09 +0200

    Add more server examples

--- server/scripts.sh
+++ server/scripts.sh
@@ -12,19 +12,30 @@ curl -i -H "Accept: application/json,text/event-stream" --data '{
      }
  }' localhost:8080/mcp
 
+ curl -i -H "Accept: application/json,text/event-stream" --data '{"jsonrpc": "2.0","id": 0,"method": "initialize","params":{"protocolVersion" : "2025-11-25","clientInfo" : {"name" : "custom","version" : "1.0"}}}' localhost:8080/mcp
+
 curl -H "Accept: application/json,text/event-stream" --data '{
   "jsonrpc" : "2.0",
   "id" : 1,
   "method" : "tools/list"
- }' localhost:8080/mcp
+ }' localhost:8080/mcp | jq ".result"
+
+ curl -H "Accept: application/json,text/event-stream" --data '{
+   "jsonrpc" : "2.0",
+   "id" : 2,
+   "method" : "tools/list",
+   "params" : {
+     "cursor" : "MjAyNi0wMy0xMFQxNjoyMTo0OS41MTcwMDY5MzdaJCQkc2FtcGxlZA=="
+   }
+
+  }' localhost:8080/mcp | jq ".result.tools[].name, .result.nextCursor"
+
 
 {
   "jsonrpc" : "2.0",
-  "id" : 1,
+  "id" : 2,
   "method" : "tools/call",
   "params" : {
-    "name" : "filereader",
-    "arguments" : {
-      "file" : "robot-readable.txt"
-    }
+    "name" : "elicitation"
   }
+}
