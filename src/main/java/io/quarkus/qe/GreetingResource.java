a36ad601458be730f7b015fd9b07181b908c27e7
Fedor Dudinsky <fdudinsk@redhat.com>
2026-06-23 15:01:17 +0200

    Reproducer for Framework bug

--- src/main/java/io/quarkus/qe/GreetingResource.java
+++ src/main/java/io/quarkus/qe/GreetingResource.java
@@ -5,7 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/")
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Path("")
 public class GreetingResource {
 
     @GET
@@ -19,5 +25,23 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @Path("/external-https")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public int client() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .GET()
+                    .uri(URI.create("https://www.redhat.com/en")).build();
+            HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+            HttpResponse<String> response = client.send(httpRequest, handler);
+            return response.statusCode();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/io/quarkus/qe/GreetingResourceTest.java
+++ src/test/java/io/quarkus/qe/GreetingResourceTest.java
@@ -1,6 +1,9 @@
 package io.quarkus.qe;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -17,5 +20,10 @@ public class GreetingResourceTest {
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
     }
-
+    @Test
+    public void shouldSendExternalRequests() {
+        Response response = given().get("/external-https");
+        Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+        Assertions.assertEquals("200", response.body().asString());
+    }
 }
